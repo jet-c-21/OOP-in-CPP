e7e42e779ca570d6c64afddee78785d5be6288ef
Jet Chien <67193495+jet-c-21@users.noreply.github.com>
2021-11-03 19:32:31 +0800

    upload get os

---
+++ get_os_name/get_os_name.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#if defined(_WIN32) || defined(_WIN64)
+const char* os = "Windows";
+#else
+#ifdef __linux
+const char* os = "Linux";
+#else
+const char* os = "Unknown (MacOS)";
+#endif
+#endif
+
+int main(void) {
+  printf("os = %s\n", os);
+  return 0;
+}
