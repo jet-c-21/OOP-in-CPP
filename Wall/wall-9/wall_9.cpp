bba8d460155c0dd0e77800b1ab0fc1520b1e5de3
Jet Chien <67193495+jet-c-21@users.noreply.github.com>
2021-12-19 17:44:44 +0800

    upload clion cmk ls

---
+++ Wall/wall-9/wall_9.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Puff on 12/16/21.
+//
+# include <iostream>
+
+using namespace std;
+
+struct Struct {
+  int Struct;
+  int Structure;
+} Structure;
+
+
+int main() {
+  Structure.Struct = 1;
+  Structure.Structure = 2;
+
+  cout << Structure.Struct << endl;
+  cout << Structure.Structure << endl;
+
+  return 0;
+}
